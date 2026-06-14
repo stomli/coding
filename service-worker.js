da9d993821cb4894d70d7434ed71f943a2218760
G. Scott <stomli@hotmail.com>
2026-06-14 15:23:28 -0700

    Add a navigation from guide and privacy back to the main page, and update the service worker to cache the new files.

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * ============================================================================
  */
 
-const CACHE_VERSION = 'orbfall-v26.0614.0234';
+const CACHE_VERSION = 'orbfall-v26.0614.0235';
 const CACHE_NAME = CACHE_VERSION;
 
 /**
